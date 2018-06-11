6a3b4b011ac0bb15fc3448f70e3bcd0df5b54c98
gomuramatsu <go.muramatsu78@gmail.com>
2018-06-10 23:59:16 -0400

    added pics to gallery

--- studiot/src/components/Gallery.js
+++ studiot/src/components/Gallery.js
@@ -2,44 +2,121 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Gallery from 'react-grid-gallery';
 import '../App.css';
-import Maki2 from '../images/maki2.jpg';
 
 const IMAGES =
     [{
+            src: "/images/front_MG_0364.jpg",
+            thumbnail: "/images/front_MG_0364_thumb.png",
+            thumbnailWidth: 400,
+            thumbnailHeight: 400
+    },
+    {
+            src: "/images/Hitomi_July_3.jpg",
+            thumbnail: "/images/Hitomi_July_3_thumb.png",
+            thumbnailWidth: 400,
+            thumbnailHeight: 400
+    },
+    {
+            src: "/images/Hit_5.jpg",
+            thumbnail: "/images/Hit_5_thumb.png",
+            thumbnailWidth: 400,
+            thumbnailHeight: 400
+    },
+    {
             src: "/images/maki2.jpg",
-            thumbnail: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_n.jpg",
-            thumbnailWidth: 320,
-            thumbnailHeight: 174
+            thumbnail: "/images/maki2_thumb.png",
+            thumbnailWidth: 400,
+            thumbnailHeight: 400
     },
     {
-            src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
-            thumbnail: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
-            thumbnailWidth: 320,
-            thumbnailHeight: 212
+            src: "/images/maki4.jpg",
+            thumbnail: "/images/maki4_thumb.png",
+            thumbnailWidth: 400,
+            thumbnailHeight: 400
     },
     {
-            src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
-            thumbnail: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg",
-            thumbnailWidth: 320,
-            thumbnailHeight: 212
+            src: "/images/model_17.jpg",
+            thumbnail: "/images/model_17_thumb.png",
+            thumbnailWidth: 400,
+            thumbnailHeight: 400
     },
     {
-            src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
-            thumbnail: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
-            thumbnailWidth: 320,
-            thumbnailHeight: 212
+            src: "/images/model_44.jpg",
+            thumbnail: "/images/model_44_thumb.png",
+            thumbnailWidth: 400,
+            thumbnailHeight: 400
     },
     {
-            src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
-            thumbnail: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_n.jpg",
-            thumbnailWidth: 320,
-            thumbnailHeight: 174
+            src: "/images/P6040232.jpg",
+            thumbnail: "/images/P6040232_thumb.png",
+            thumbnailWidth: 400,
+            thumbnailHeight: 400
     },
     {
-            src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
-            thumbnail: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg",
-            thumbnailWidth: 320,
-            thumbnailHeight: 212
+            src: "/images/Picture_144.jpg",
+            thumbnail: "/images/Picture_144_thumb.png",
+            thumbnailWidth: 400,
+            thumbnailHeight: 400
+    },
+    {
+            src: "/images/sora572A4436.jpg",
+            thumbnail: "/images/sora572A4436_thumb.png",
+            thumbnailWidth: 400,
+            thumbnailHeight: 400
+    },
+    {
+            src: "/images/Sumi_f3.jpg",
+            thumbnail: "/images/Sumi_f3_thumb.png",
+            thumbnailWidth: 400,
+            thumbnailHeight: 400
+    },
+    {
+            src: "/images/top_MG_0079.jpg",
+            thumbnail: "/images/top_MG_0079_thumb.png",
+            thumbnailWidth: 400,
+            thumbnailHeight: 400
+    },
+    {
+            src: "/images/IMG_0818.jpg",
+            thumbnail: "/images/IMG_0818_thumb.png",
+            thumbnailWidth: 400,
+            thumbnailHeight: 400
+    },
+    {
+            src: "/images/IMG_1084.jpg",
+            thumbnail: "/images/IMG_1084_thumb.png",
+            thumbnailWidth: 400,
+            thumbnailHeight: 400
+    },
+    {
+            src: "/images/IMG_1104.jpg",
+            thumbnail: "/images/IMG_1104_thumb.png",
+            thumbnailWidth: 400,
+            thumbnailHeight: 400
+    },
+    {
+            src: "/images/IMGP0075.jpg",
+            thumbnail: "/images/IMGP0075_thumb.png",
+            thumbnailWidth: 400,
+            thumbnailHeight: 400
+    },
+    {
+            src: "/images/IMGP0232.jpg",
+            thumbnail: "/images/IMGP0232_thumb.png",
+            thumbnailWidth: 400,
+            thumbnailHeight: 400
+    },
+    {
+            src: "/images/P1010284.jpg",
+            thumbnail: "/images/P1010284_thumb.png",
+            thumbnailWidth: 400,
+            thumbnailHeight: 400
+    },
+    {
+            src: "/images/que.jpg",
+            thumbnail: "/images/que_thumb.png",
+            thumbnailWidth: 400,
+            thumbnailHeight: 400
     }]
 
 class GalleryPage extends Component {  
